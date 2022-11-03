fd3f06df85780e3a2e03d0e1fd308ec52e0add89
Juliana Ribeiro <silva.jurib@gmail.com>
2022-11-03 03:13:54 -0300

    feat: add imc-calc

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-com-nodejs/dia-01-nodejs-runtime-assincrono/criando-um-script-simples/index.js
+++
@@ -1,6 +0,0 @@
-const readline = require('readline-sync');
-
-const name = readline.question(`What's your name?`);
-const age = readline.question(`How old are you?`);
-
-console.log(`Hello, ${name}! You are ${age} years old!`);
