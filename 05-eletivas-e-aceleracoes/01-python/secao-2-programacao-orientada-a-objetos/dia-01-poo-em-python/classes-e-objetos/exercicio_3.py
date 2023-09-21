1936c17b0d64644ebed2bd3863f04ccc97085892
Juliana Ribeiro <silva.jurib@gmail.com>
2023-09-21 10:18:02 -0300

    feat: add classes and objects exercises

---
+++ 05-eletivas-e-aceleracoes/01-python/secao-2-programacao-orientada-a-objetos/dia-01-poo-em-python/classes-e-objetos/exercicio_3.py
@@ -0,0 +1,6 @@
+"""Crie a classe Database.
+Não precisa colocar nada dentro dela, somente a palavra reservada pass """
+
+
+class Database:
+    pass
---
+++ 05-eletivas-e-aceleracoes/01-python/secao-2-programacao-orientada-a-objetos/dia-01-poo-em-python/classes-e-objetos/exercicio_4.py
@@ -0,0 +1,6 @@
+from exercicio_3 import Database
+
+"""Crie dois objetos da classe Database,
+criada no exercício anterior, instanciando-os"""
+object_1 = Database()
+object_2 = Database()
